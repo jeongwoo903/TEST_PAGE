c25075747043656dff6f9f3f66ba614a0c6c278d
jeongwoo903 <www8565@naver.com>
2022-02-26 18:40:20 +0900

    ..
    ..

---
+++ scripts/indexScript.js
@@ -0,0 +1,11 @@
+(function () {
+    let wrapper = null;
+
+    window.addEventListener('DOMContentLoaded', function () {
+        wrapper = document.querySelector(".wrapper");
+    });
+    window.onload = function () {
+
+        wrapper.style.opacity = "1";
+    };
+})();
