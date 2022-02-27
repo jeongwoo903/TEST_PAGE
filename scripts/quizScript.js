e91ba3462a23b1da04ab82a58faadad3c7720883
jeongwoo903 <www8565@naver.com>
2022-02-27 11:31:28 +0900

    Import Commit
    - transition 없앰..!!!!!!
    - 기능 개선
    
    - quiz list 모듈화 필요
    - button_wrapper 문제 해결 필요

--- scripts/quizScript.js
+++ scripts/quizScript.js
@@ -1,50 +1,52 @@
 const ulElem = document.querySelector("ul.content_wrapper");
+// 페이지 보여주기
+ulElem.childNodes[1].classList.add("show");
 // 버튼 누르면 앞으로 가기
-let page_value = 0;
 const btn = document.querySelectorAll(".btn");
+const btn_wrapper = document.querySelector(".button_wrapper");
 const btn_up = document.querySelector("button.up");
 const btn_down = document.querySelector("button.down");
 // 버튼 누르면 뒤로 가기
 const btn_back = document.querySelector("button.back");
-// 페이지 수 세기
+// 페이지 수 세기 -> 1이하가 안되게끔..
 let count = 1;
 const counter = document.querySelector("p.count");
 // 페이지 게이지
 let gauge = 10;
 const gaugebar = document.querySelector("div.gauge");
 
-const page_width = ulElem.childNodes[1].clientWidth;
+// 버튼 상호작용 (체크된 버튼 / 게이지바 / 페리지 카운트)
+btn_wrapper.addEventListener("click", e => {
+    const tg = e.target;
+    const currentItem = document.querySelector(".check");
 
-for (const btn_child of btn) {
-    btn_child.addEventListener("click", () => {
-        page_value += page_width;
-        btn_back.style.visibility = "visible";
-        ulElem.style.transform = `translate(-${page_width}px)`;
-        gaugebar.style.width = `${gauge += 10}%`;
-    });
-}
+    ulElem.childNodes[2 * count - 1].classList.remove("show");
 
-btn_up.addEventListener("click", () => {
     count++;
     counter.innerHTML = `${count}/10`;
-});
 
-btn_down.addEventListener("click", () => {
-    count--;
-    counter.innerHTML = `${count}/10`;
-});
+    ulElem.childNodes[2 * count - 1].classList.add("show");
 
-if (page_value == 0) btn_back.style.visibility = "hidden";
+    if (currentItem) {
+        currentItem.classList.remove("check");
+    }
+
+    if (tg.classList.contains("btn")) {
+        tg.classList.add("check");
+    }
+
+    btn_back.style.visibility = "visible";
+    gaugebar.style.width = `${gauge += 10}%`;
+});
 
 btn_back.addEventListener("click", () => {
-    if (page_value == ulElem.childNodes[1].clientWidth) btn_back.style.visibility = "hidden";
-
-    page_value -= page_width;
-    ulElem.style.transform = `translate(-${page_value}px)`;
     count--;
     counter.innerHTML = `${count}/10`;
+    ulElem.childNodes[2 * count - 1].classList.add("show");
+
+    if (count == 1) {
+        btn_back.style.visibility = "hidden";
+    }
 
     gaugebar.style.width = `${gauge -= 10}%`;
 });
-
-console.log(count);
