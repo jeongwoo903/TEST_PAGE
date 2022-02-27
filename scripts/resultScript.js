70a3d3d4fdd6baab2e15c01b202554a5b2ab7a1a
jeongwoo903 <www8565@naver.com>
2022-02-28 01:22:58 +0900

    Update resultScript.js

--- scripts/resultScript.js
+++ scripts/resultScript.js
@@ -1,8 +1,4 @@
 const pElem = document.querySelector(".result");
-let mbti_regexp = document.location.href.match(/[IENSTFJP]/g);
-let mbti = "";
-for (let i = 0; i < mbti_regexp.length; i++) {
-    mbti += mbti_regexp[i];
-}
+let mbti = document.location.href.split("#");
 
-pElem.innerHTML = `재미로 본 당신의 MBTI는 ${mbti}입니다.`;
+pElem.innerHTML = `재미로 본 당신의 MBTI는 ${mbti[1]}입니다.`;
